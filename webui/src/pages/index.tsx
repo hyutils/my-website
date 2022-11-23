5dee9da6fb0b756661fba4d314f3c3abacc34293
hy918 <23854996732qq.com>
2022-11-23 11:04:48 +0800

    初始化

---
+++ webui/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from 'antd';
+import './index.less';
+
+export default function index() {
+  return (
+    <div className='root'>
+      <div className='title'>index</div>
+      <Button type='primary'>点击</Button>
+    </div>
+  )
+}
